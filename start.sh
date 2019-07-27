19993d0ea3b6e217f78dcaf6f5abc1d63a22e866
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-27 22:39:33 +1000

    Auto restart git-cache-server when it exited

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-git-cache-http-server &
+start_server() {
+    while true; do
+        git-cache-http-server
+	echo "git-cache-http-server exited with $?"
+    done
+}
+
+start_server &
 
 source /root/git-gc.sh
